845e259bfcc38dc0d79b02ec6d628b30dbec3f5b
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-04-05 06:37:08 +0630

    update

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 // import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import React, { useCallback } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/LoginScreen";
+import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
 
+// SplashScreen.preventAutoHideAsync();
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+  });
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -18,12 +36,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,53 @@
-import { View, Text } from "react-native";
-import { Button } from "@rneui/themed";
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Button, Image } from "@rneui/themed";
+import { primaryColor } from "../constants";
+import logo from "../assets/logo.png";
 
 export default function LoginScreen() {
   return (
     <View
       style={{
-        height: 500,
+        height: "100%",
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#fff",
       }}
     >
+      <Image
+        source={logo}
+        containerStyle={{
+          width: "100%",
+          height: "100%",
+          transform: "scale(0.5)",
+        }}
+      />
       <Text>Login Screen</Text>
-
-      <Button title="Solid" />
-      <Button title="Outline" type="outline" />
-      <Button title="Clear" type="clear" />
+      <View style={styles.loginBtnContainer}>
+        <Button
+          title="Login"
+          buttonStyle={styles.loginBtn}
+          radius={15}
+          titleStyle={styles.loginBtnTitle}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  loginBtnContainer: {
+    position: "absolute",
+    bottom: 52.17,
+    width: "100%",
+    paddingHorizontal: 37,
+  },
+  loginBtnTitle: {
+    fontSize: 16,
+    fontFamily: "Poppins-Medium",
+  },
+  loginBtn: {
+    backgroundColor: primaryColor,
+    height: 50.83,
+  },
+});
